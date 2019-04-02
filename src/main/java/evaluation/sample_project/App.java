d6e21fb51758da6ab068bc1f3367f24792b70546
jotecasve <39869907+jotecasve@users.noreply.github.com>
2019-04-02 16:02:37 -0500

    this is a test for evaluation

--- src/main/java/evaluation/sample_project/App.java
+++ src/main/java/evaluation/sample_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Last 3 time!" );
+        System.out.println( "Hello World! Last 4 and evaluation time!" );
     }
 }
