b6c9e4d19844dbc6987c75765104d2dc0559ff97
jotecasve <39869907+jotecasve@users.noreply.github.com>
2019-04-02 17:13:51 -0500

    testing deployments

--- src/main/java/evaluation/sample_project/App.java
+++ src/main/java/evaluation/sample_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Last 4 and evaluation time with webhooks." );
+        System.out.println( "Hello World! Last 4 and evaluation time." );
     }
 }
