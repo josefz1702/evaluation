1f38ee70a7edefd198ce80f6e43f6c4c2f30b9d3
jotecasve <39869907+jotecasve@users.noreply.github.com>
2019-04-02 18:07:17 -0500

    Adding api testing

--- src/test/java/evaluation/sample_project/AppTest.java
+++ src/test/java/evaluation/sample_project/AppTest.java
@@ -1,6 +1,15 @@
 package evaluation.sample_project;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 
 public class AppTest {
@@ -18,5 +27,22 @@ public class AppTest {
     		int a=3,b=5;
     		int result = app.sumar(a, b);
     		assertEquals(8, result);
+    }
+    
+    @Test
+    public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived()
+      throws ClientProtocolException, IOException {
+      
+       // Given
+       String name = "jotecasve";
+       HttpUriRequest request = new HttpGet( "https://api.github.com/users/" + name );
+     
+       // When
+       HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+     
+       // Then
+       assertEquals(
+    		     httpResponse.getStatusLine().getStatusCode(),
+    		     200);
     }
 }
