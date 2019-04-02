1574505a8a3a17b25f56395e9836e6a67328ebb7
jotecasve <39869907+jotecasve@users.noreply.github.com>
2019-04-02 17:27:28 -0500

    Update App.java

--- src/main/java/evaluation/sample_project/App.java
+++ src/main/java/evaluation/sample_project/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Last evaluation time and testing." );
+        System.out.println( "Hello World!" );
+        String result = concatenate("Hello","World! ");
+        System.out.println(result+ sumar(5,6) + " times.");
+    }
+    
+    public static String concatenate(String one, String two){
+        return one + two;
+    }
+    
+    public static int sumar(int num1, int num2) {
+    		return num1+num2;
     }
 }
