7de3f018a46233fb9ccd1bcc1dd3d37c20e9c164
jotecasve <39869907+jotecasve@users.noreply.github.com>
2019-04-02 15:59:49 -0500

    Update App.java

--- src/main/java/evaluation/sample_project/App.java
+++ src/main/java/evaluation/sample_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Second time!" );
+        System.out.println( "Hello World! Last 3 time!" );
     }
 }
